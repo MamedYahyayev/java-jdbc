b9e08e7fb8e285550325fcfa39a6c5ef73b6bcfd
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-13 13:27:43 +0400

    MySqlDataSource inserted and caching enabled

--- src/az/adnsu/dao/DatabaseHelper.java
+++ src/az/adnsu/dao/DatabaseHelper.java
@@ -8,21 +8,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseHelper {
 
 	private static HikariDataSource dataSource;
+	private static MysqlDataSource mySqlDataSource = new MysqlDataSource();
+	private static String driver;
+	private static String url;
+	private static String username;
+	private static String password;
 
 	public static Connection getConnection() throws Exception {
-		Properties property = loadPropertyFile();
-
-		String driver = property.getProperty("db.driver");
-		String url = property.getProperty("db.url");
-		String username = property.getProperty("db.user");
-		String password = property.getProperty("db.password");
-
+		loadPropertyFile();
 		Class.forName(driver);
 		Connection connection = DriverManager.getConnection(url, username, password);
 		return connection;
@@ -33,27 +33,38 @@ public class DatabaseHelper {
 		return dataSource.getConnection();
 	}
 
+	public static Connection getMySqlConnection()
+			throws SQLException, FileNotFoundException, IOException, ClassNotFoundException {
+		loadPropertyFile();
+		Class.forName(driver);
+		mySqlDataSource.setUser(username);
+		mySqlDataSource.setPassword(password);
+		mySqlDataSource.setUrl(url);
+		mySqlDataSource.setCachePrepStmts(true);
+		mySqlDataSource.setUseServerPrepStmts(true);
+		mySqlDataSource.setPrepStmtCacheSize(400);
+		mySqlDataSource.setPrepStmtCacheSqlLimit(1000);
+		return mySqlDataSource.getConnection();
+	}
+
 	private static HikariConfig getHikariConfig() throws FileNotFoundException, IOException {
-		Properties property = loadPropertyFile();
-
-		String driver = property.getProperty("db.driver");
-		String url = property.getProperty("db.url");
-		String username = property.getProperty("db.user");
-		String password = property.getProperty("db.password");
-
+		loadPropertyFile();
 		HikariConfig config = new HikariConfig();
 		config.setJdbcUrl(url);
 		config.setUsername(username);
 		config.setPassword(password);
 		config.setDriverClassName(driver);
 		config.setAutoCommit(false);
-		
 		return config;
 	}
 
 	private static Properties loadPropertyFile() throws IOException, FileNotFoundException {
 		Properties property = new Properties();
 		property.load(new FileReader("database.properties"));
+		driver = property.getProperty("db.driver");
+		url = property.getProperty("db.url");
+		username = property.getProperty("db.user");
+		password = property.getProperty("db.password");
 		return property;
 	}
 
